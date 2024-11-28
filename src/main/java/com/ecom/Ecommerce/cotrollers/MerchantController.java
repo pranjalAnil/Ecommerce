f11701e7295a695c7e0fa6824e6bbee64bcdbdbc
Suchitn7 <suchitn7@gmail.com>
2024-11-28 12:42:59 +0530

    Products crud

--- src/main/java/com/ecom/Ecommerce/cotrollers/MerchantController.java
+++ src/main/java/com/ecom/Ecommerce/cotrollers/MerchantController.java
@@ -1,17 +1,25 @@
 package com.ecom.Ecommerce.cotrollers;
 
 import com.ecom.Ecommerce.payloads.MerchantDto;
+import com.ecom.Ecommerce.payloads.ProductsDto;
 import com.ecom.Ecommerce.services.MerchantService;
+import com.ecom.Ecommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/merchant")
 public class MerchantController {
+
     @Autowired
     MerchantService merchantService;
+
+    @Autowired
+    ProductService productService;
 
     @PostMapping("/createAcc")
     public ResponseEntity<?> createMerchant(@RequestBody MerchantDto merchantDto){
@@ -29,5 +37,30 @@ public class MerchantController {
         return new ResponseEntity<>(merchantService.deleteAccount(email),HttpStatus.OK);
     }
 
+    @PostMapping("/add-product")
+    public ResponseEntity<ProductsDto> addProducts(@RequestBody ProductsDto productsDto) {
+        ProductsDto productsDto1 = productService.addProducts(productsDto);
+        return new ResponseEntity<>(productsDto1, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/all-products")
+    public ResponseEntity<?> getAllProducts() {
+        List<ProductsDto> all = productService.getAllProducts();
+        if (all != null && !all.isEmpty()) {
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(all, HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/update-product/{prodId}")
+    public ResponseEntity<ProductsDto> updateProduct(@RequestBody ProductsDto productsDto, @PathVariable int prodId ){
+        return new ResponseEntity<>(productService.updateProduct(productsDto,prodId), HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/deleteProd/{prodId}")
+    public ResponseEntity<?> deleteProduct(@PathVariable int prodId) {
+        return new ResponseEntity<>(productService.deleteProducts(prodId), HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/ecom/Ecommerce/services/Impl/ProductServiceImpl.java
+++ src/main/java/com/ecom/Ecommerce/services/Impl/ProductServiceImpl.java
@@ -1,30 +1,58 @@
 package com.ecom.Ecommerce.services.Impl;
+import com.ecom.Ecommerce.entities.Products;
 import com.ecom.Ecommerce.payloads.ProductsDto;
+import com.ecom.Ecommerce.repo.ProductRepo;
 import com.ecom.Ecommerce.services.ProductService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
+    @Autowired
+    ProductRepo productRepo;
+
     @Override
     public ProductsDto addProducts(ProductsDto productsDto) {
-        return null;
+        Products products = new Products();
+        BeanUtils.copyProperties(productsDto, products);
+        productRepo.save(products);
+        return productsDto;
     }
 
     @Override
     public ProductsDto updateProduct(ProductsDto productsDto, int prodId) {
-        return null;
+        Products products = productRepo.findById(prodId).orElseThrow();
+        products.setProdName(productsDto.getProdName());
+        products.setAbout(productsDto.getAbout());
+        products.setImageName(productsDto.getImageName());
+        products.setNumOfProducts(productsDto.getNumOfProducts());
+        productRepo.save(products);
+        BeanUtils.copyProperties(products, productsDto);
+        return productsDto;
     }
 
     @Override
     public List<ProductsDto> getAllProducts() {
-        return null;
+        List<Products> list = productRepo.findAll();
+        List<ProductsDto> list1 = new ArrayList<>();
+        for (Products products : list){
+            ProductsDto productsDto = new ProductsDto();
+            BeanUtils.copyProperties(products, productsDto);
+            list1.add(productsDto);
+        }
+        System.out.println(list1);
+         return list1;
+
     }
 
     @Override
-    public ProductsDto deleteProducts(int productId) {
-        return null;
+    public String deleteProducts(int prodId) {
+        productRepo.deleteById(prodId);
+        return "Product deleted";
     }
 }
